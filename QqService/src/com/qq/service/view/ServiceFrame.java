672f0c4f51819b578ee5dc5bd1c9e1076704bca5
L_zhoutao <1217396582@qq.com>
2017-12-15 21:47:56 +0800

    service Frame

--- QqService/src/com/qq/service/view/ServiceFrame.java
+++ QqService/src/com/qq/service/view/ServiceFrame.java
@@ -11,6 +11,9 @@ import javax.swing.border.EmptyBorder;
 import com.qq.service.model.MyQqService;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class ServiceFrame extends JFrame implements ActionListener {
 
@@ -21,6 +24,8 @@ public class ServiceFrame extends JFrame implements ActionListener {
 	
 	private JPanel contentPane;
 	private JButton openButton, closeButton;
+	private JTextField adminTextField;
+	private JTextField passwdTextField;
 
 	/**
 	 * Launch the application.
@@ -42,8 +47,14 @@ public class ServiceFrame extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public ServiceFrame() {
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 351, 175);
+		setBounds(100, 100, 360, 227);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -53,13 +64,32 @@ public class ServiceFrame extends JFrame implements ActionListener {
 		openButton = new JButton("启动服务器");
 		// 设置登录为默认按钮
 		this.getRootPane().setDefaultButton(openButton);
-		openButton.setBounds(28, 36, 119, 48);
+		openButton.setBounds(43, 120, 119, 48);
 		contentPane.add(openButton);
 		openButton.addActionListener(this);
 		//关闭服务器
-		closeButton = new JButton("关闭服务器");
-		closeButton.setBounds(194, 36, 111, 48);
+		closeButton = new JButton("退出服务器");
+		closeButton.setBounds(193, 120, 111, 48);
 		contentPane.add(closeButton);
+		closeButton.addActionListener(this);
+		
+		JLabel adminLabel = new JLabel("管理员帐号：");
+		adminLabel.setBounds(30, 23, 90, 39);
+		contentPane.add(adminLabel);
+		
+		adminTextField = new JTextField();
+		adminTextField.setBounds(126, 30, 151, 24);
+		contentPane.add(adminTextField);
+		adminTextField.setColumns(10);
+		
+		JLabel passwdLabel = new JLabel("密码：");
+		passwdLabel.setBounds(73, 68, 45, 39);
+		contentPane.add(passwdLabel);
+		
+		passwdTextField = new JTextField();
+		passwdTextField.setBounds(126, 75, 151, 24);
+		contentPane.add(passwdTextField);
+		passwdTextField.setColumns(10);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setTitle("服务器");
@@ -69,11 +99,13 @@ public class ServiceFrame extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO 自动生成的方法存根
-		if(e.getSource() == openButton)
-		{
+		if(e.getSource() == openButton)	{
 			this.setVisible(false);
 			new ManagerFrame();
 			new MyQqService();
+		}
+		else if(e.getSource() == closeButton) {
+			this.dispose();
 		}
 	}
 }
